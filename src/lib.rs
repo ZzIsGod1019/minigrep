16b95ab6cee20520e67b8a99b36e18a8db13401f
zhuzhe <1498852723@qq.com>
2021-07-04 02:11:40 +0800

    代码调整优化

--- src/lib.rs
+++ src/lib.rs
@@ -39,25 +39,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query:&str, contents:&'a str) -> Vec<&'a str>{
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line.trim());
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 pub struct Config {
     pub query: String,
     pub filename: String,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();// 忽略第一个值
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename string"),
+        };
     
         Ok(Config { query, filename })
     }
